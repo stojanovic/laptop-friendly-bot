59ecb6aeb9b34d07d5d13a6a227bb771850971dd
Slobodan Stojanovic <slobodan@cloudhorizon.com>
2016-11-17 13:13:28 +0100

    Add menu

--- bot.js
+++ bot.js
@@ -1,8 +1,21 @@
-'use strict';
+'use strict'
 
-var botBuilder = require('claudia-bot-builder');
+const botBuilder = require('claudia-bot-builder')
+const viberTemplate = botBuilder.viberTemplate
 
-module.exports = botBuilder(message => {
-  return `Hello! You sent ${message.text}`;
-});
+function mainMenu() {
+  return viberTemplate.Text(`Here's what can I do for you:`)
+    .addReplyKeyboard()
+    .addKeyboardButton('Locations', 'reply', 'LOCATIONS', 3, 2, '#f6d95e')
+    .addKeyboardButton('About LaptopFriendly', 'reply', 'ABOUT_LF', 3, 2, '#343434')
+    .addKeyboardButton('Packages', 'reply', 'PACKAGES', 3, 2, '#343434')
+    .addKeyboardButton('LaptopFriendly Website', 'open-url', 'https://laptopfriendly.co', 3, 2, '#f6d95e')
+    .get()
+}
 
+module.exports = botBuilder(() => {
+  return [
+    `Hello! I am Laptop Friendly bot`,
+    mainMenu()
+  ]
+})
